06cebfc80daf1f2b9f6455cd615598d2958dd6d5
Ardaerd <arderd@outlook.com>
2022-08-11 13:17:02 +0300

    service is added

---
+++ service/service/Controllers/StockSystemController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockSystemController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product
+            {
+                Price = 5.5,
+                Quantity = 10,
+                Barcode = "23419414",
+                CashRegister_No = 1,
+                IrsaliyeDate = DateTime.Now
+            }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Product>>> sellProduct(Product product)
+        {
+            products.Add(product);
+            return Ok(products);
+        }
+    }
+}
---
+++ service/service/Stock.cs
@@ -0,0 +1,11 @@
+﻿namespace service
+{
+    public class Product
+    {
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string Barcode { get; set; }
+        public int CashRegister_No { get; set; }
+        public DateTime IrsaliyeDate { get; set; }
+    }
+}
